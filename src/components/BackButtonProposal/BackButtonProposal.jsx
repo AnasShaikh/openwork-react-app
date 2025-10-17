99fb61fffb5d40f523daa6359de3fd526c24453d
Krishna Prasath <kp872003@gmail.com>
2025-10-17 17:45:05 +0530

    fix-bugs

--- src/components/BackButtonProposal/BackButtonProposal.jsx
+++ src/components/BackButtonProposal/BackButtonProposal.jsx
@@ -1,25 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./BackButtonProposal.css";
+import "./BackButtonProposal.css"; // We'll update this CSS
 
-const BackButtonProposal = ({ to = "/dao", title = "Back", imgSrc = "/button.svg", style }) => (
-  <div className="form-navigationProposal" style={style}>
-    <Link to={to} className="backButtonProposal">
-      <img
-        src={imgSrc}
-        alt="Back"
-        className="backIconProposal"
-      />
-    </Link>
-    <div className="formTitleProposal">{title}</div>
-    <Link to={to} className="backButtonProposal" style={{visibility: 'hidden'}}>
-      <img
-        src={imgSrc}
-        alt="Back"
-        className="backIconProposal"
-      />
-    </Link>
-  </div>
+// We'll simplify the props, as 'title' will no longer be part of this component's render
+const BackButtonProposal = ({ to = "/dao", imgSrc = "/button.svg" }) => (
+  // The outer div 'form-navigationProposal' should now be the container for the entire header,
+  // so we won't wrap just the back button in it here.
+  // This component will ONLY render the back button link.
+  <Link to={to} className="backButtonProposal">
+    <img
+      src={imgSrc}
+      alt="Back" // Still important for accessibility
+      className="backIconProposal" // Keep this class for sizing/styling
+    />
+  </Link>
 );
 
 export default BackButtonProposal;
